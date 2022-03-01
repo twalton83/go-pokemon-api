a9e59cb1e10553d7e3f7ed79c98985612b58b8a0
Tatiana <hello@tatianacodes.com>
2022-02-28 20:38:46 -0800

    Set up basic route to GET pokemon:

--- main.go
+++ main.go
@@ -5,18 +5,46 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"log"
+	"io"
 )
 
 func main() {
   fmt.Println("Starting server...")
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", RootHandler)
+	r.HandleFunc("/", RootHandler).
+		Methods("GET")
+	r.HandleFunc("/pokemon/{name}", PokemonGetHandler).
+		Methods("GET")
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe("localhost:8080", r))
+	log.Fatal(http.ListenAndServe("localhost:8000", r))
 }
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("HELLO WORLD!"))
 }
+
+func PokemonGetHandler(w http.ResponseWriter, r *http.Request) {
+	// take POKEMON from query param
+	pokemon := mux.Vars(r)["name"]
+	// fetch PokeApi
+	fmt.Println(pokemon)
+	resp, err := http.Get("https://pokeapi.co/api/v2/pokemon/" + pokemon)
+
+	if err != nil {
+		log.Fatalln(err)
+	} else {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		} else {
+			sb := string(body)
+			w.Write([]byte(sb))
+		}
+	}
+
+
+}
+	
+
 
